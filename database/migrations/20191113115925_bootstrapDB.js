a39157fb08b9b6f9e4627203f3de6f980e558511
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-11-13 12:03:40 -0800

    created knexfile, migration, and ran migration

---
+++ database/migrations/20191113115925_bootstrapDB.js
@@ -0,0 +1,16 @@
+
+exports.up = function (knex) {
+    return knex.schema.createTable('users', users => {
+        users.increments();
+        users
+            .string('username')
+            .notNullable()
+            .unique();
+        users.string('password').notNullable();
+        users.string('department');
+    })
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('users');
+};
