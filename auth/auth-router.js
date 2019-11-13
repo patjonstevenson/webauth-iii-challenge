acda198a6d66f2ae2ea84166696b9bb897e8e061
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-11-13 12:38:15 -0800

    wrote users-model, started fleshing out handler for post to /register in auth-router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,9 +1,23 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require("../database/db-config");
+const {validateUser} = require('../users/users-helpers');
 
 router.post('/register', async (req, res) => {
+    let user = req.body;
+    const validateResults = validateUser(user);
+    
+    if (validateResults.isSuccessful) {
+        const hash = bcrypt.hashSync(user.password, 10);
+        user = {
+            ...user,
+            password: hash
+        };
 
+        
+    }
 });
 
 router.post('/login', async (req, res) => {
